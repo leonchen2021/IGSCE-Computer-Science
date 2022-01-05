72279aea9b0b35292ee73700678d35e88fdf5b24
Leon <leon.chen.kia@gmail.com>
2022-01-05 12:03:16 +0800

    LOL

--- data strucuture/working.py
+++ data strucuture/working.py
@@ -1,3 +1,18 @@
+#the exam coding for list
 has_participated = [True, True, False, True, False, False, True]
+has_participated.sort()
+has_participated.reverse()
+print (has_participated)
 number_of_participation = has_participated.count(True)
-print("Number of people who did homework", number_of_participation)
+print("Number of people who participated", number_of_participation)
+
+#the exam coding for dictionaries
+student_scores = dict(Amy=89, Leon=88, Mike=85, Charlie=86, Amini=100)
+print (student_scores)
+student_scores.get("Amini")
+print ("Mr. Amini", student_scores)
+#create the dictionary that stores the students names and their pet's names
+student_pets = dict(Adrain=["pet1", "pet2", "pet3"], Leon=["pet1", "pet2", "pet3"], Amini=["pet1", "pet2", "pet3"])
+student_pets.get("Amini")
+for pets in student_pets:
+    print(pets)
