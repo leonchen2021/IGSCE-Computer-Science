c9a09f3addb78c99cf941baac7cb18bf57164279
Leon <leon.chen.kia@gmail.com>
2022-01-07 09:38:58 +0800

    Hello

--- data strucuture/working.py
+++ data strucuture/working.py
@@ -9,10 +9,10 @@ print("Number of people who participated", number_of_participation)
 #the exam coding for dictionaries
 student_scores = dict(Amy=89, Leon=88, Mike=85, Charlie=86, Amini=100)
 print (student_scores)
-student_scores.get("Amini")
-print ("Mr. Amini", student_scores)
+score = student_scores.get("Amini")
+print ("Mr. Amini", score)
 #create the dictionary that stores the students names and their pet's names
 student_pets = dict(Adrain=["pet1", "pet2", "pet3"], Leon=["pet1", "pet2", "pet3"], Amini=["pet1", "pet2", "pet3"])
-student_pets.get("Amini")
-for pets in student_pets:
-    print(pets)
+pet_name = student_pets.get("Amini")
+print (pet_name)
+
