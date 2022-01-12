0af74f180aa7f1a0203bb1bcfd15d4c10254714e
Leon <leon.chen.kia@gmail.com>
2022-01-12 09:43:14 +0800

    good morning

--- data strucuture/working.py
+++ data strucuture/working.py
@@ -35,24 +35,20 @@ for new_bocket in list_two:
 
 
 #working for binary search 
-sorted_list = [1, 3, 5, 7, 9, 8, 10]
-target = 5
+sorted_list = [1, 4, 6, 9, 11, 14, 15, 16, 18, 19, 20]
+target = 1
 def binary_search(sorted_list, target):
     left_idx = 0
     right_idx = len(sorted_list) - 1
     print(right_idx)
-    
     while(left_idx <= right_idx):
-        middle_idx = int((right_idx + left_idx) / 2)
+        middle_idx = int((left_idx + right_idx) / 2) 
         if(sorted_list[middle_idx] == target):
             return middle_idx
         elif(target < sorted_list[middle_idx]):
             right_idx = middle_idx - 1
         else:
             left_idx = middle_idx + 1
-    return - 1
+    return -1 
 result = binary_search(sorted_list, target)
-print('the target number is the list index:', result)
-    
-
-
+print('the target number is at list index:', result)
